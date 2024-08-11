f9a969a6aa426e9091da231608bc52ba24b05361
vesper-arch <ej1mahoney@gmail.com>
2024-08-11 15:59:17 -0400

    Add missing packages

--- Scripts/install_programs.sh
+++ Scripts/install_programs.sh
@@ -5,7 +5,7 @@ sudo pacman -Syu
 sleep 1
 
 sudo pacman -S steam neovim oh-my-posh zsh kitty waybar gimp swaync grim slurp fuzzel discord yay \
-    swww obsidian
+    swww obsidian wl-clipboard wlogout zoxide
 
 sleep 1
 
