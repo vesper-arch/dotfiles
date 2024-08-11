3f9f615381b91d187d11164270f9a82c4302eccf
vesper-arch <ej1mahoney@gmail.com>
2024-08-11 15:56:05 -0400

    Add install script

---
+++ Scripts/install_programs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo pacman -Syu
+
+sleep 1
+
+sudo pacman -S steam neovim oh-my-posh zsh kitty waybar gimp swaync grim slurp fuzzel discord yay \
+    swww obsidian
+
+sleep 1
+
+# Aur packages
+yay -S ckb-next hyprland-git cava
