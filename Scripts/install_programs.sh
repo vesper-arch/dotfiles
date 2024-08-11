6f31b6733e2fd4379876f1adb00a9d483ffec282
vesper-arch <ej1mahoney@gmail.com>
2024-08-11 16:09:34 -0400

    Update scripts

--- Scripts/install_programs.sh
+++ Scripts/install_programs.sh
@@ -7,6 +7,10 @@ sleep 1
 sudo pacman -S steam neovim oh-my-posh zsh kitty waybar gimp swaync grim slurp fuzzel discord yay \
     swww obsidian wl-clipboard wlogout zoxide
 
+sudo pacman -S tff-jetbrains-mono-nerd
+
+sudo pacman -S tff-jetbrains-mono-nerd
+
 sleep 1
 
 # Aur packages
---
+++ Scripts/set_wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+swww img --transition-type outer --transition-pos 0.854,0.977 --transition-step 90 ~/Wallpapers/Wallpaper2.png
