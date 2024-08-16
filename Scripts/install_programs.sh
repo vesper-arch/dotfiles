9a6bc1373774563cbf7502903964978aa311356b
vesper-arch <vesper-dev.repacking282@passmail.net>
2024-08-16 12:40:48 -0400

    Rework install script

--- Scripts/install_programs.sh
+++ Scripts/install_programs.sh
@@ -4,14 +4,12 @@ sudo pacman -Syu
 
 sleep 1
 
-sudo pacman -S steam neovim oh-my-posh zsh kitty waybar gimp swaync grim slurp fuzzel discord yay \
-    swww obsidian wl-clipboard wlogout zoxide
+sudo pacman -S steam neovim zsh kitty waybar gimp swaync grim slurp fuzzel discord yay \
+    swww obsidian wl-clipboard zoxide
 
-sudo pacman -S tff-jetbrains-mono-nerd
-
-sudo pacman -S tff-jetbrains-mono-nerd
+sudo pacman -S ttf-jetbrains-mono-nerd
 
 sleep 1
 
 # Aur packages
-yay -S ckb-next hyprland-git cava
+yay -S ckb-next hyprland-git cava oh-my-posh
