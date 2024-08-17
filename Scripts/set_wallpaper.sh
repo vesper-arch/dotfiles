bcbe513a0b2a72f2368d0eea0c734e23fa08119b
vesper-arch <vesper-dev.repacking282@passmail.net>
2024-08-17 14:11:33 -0400

    Made set_wallpaper take a path argument

--- Scripts/set_wallpaper.sh
+++ Scripts/set_wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-swww img --transition-type outer --transition-pos 0.854,0.977 --transition-step 90 ~/Wallpapers/Wallpaper2.png
+swww img --transition-type outer --transition-pos 0.854,0.977 --transition-step 90 $1
