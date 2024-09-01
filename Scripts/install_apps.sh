f0589af140b5bf4b341c72c340270cca148c3448
vesper-arch <vesper-arch@proton.me>
2024-09-01 13:32:28 -0400

    Add missing packages

--- Scripts/install_apps.sh
+++ Scripts/install_apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo pacman -S steam discord obsidian gimp
+sudo pacman -S steam discord obsidian gimp obs-studio ark
+
+sudo pacman -S noto-fonts-cjk noto-fonts-emoji
 
 yay -S ckb-next zen-browser-bin
 
--- Scripts/install_desktop.sh
+++ Scripts/install_desktop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S swww fuzzel swaync
+sudo pacman -S hyprland swww fuzzel swaync
 
 # This is required for the cava module to work as it is not included in the pacman version
 yay -S waybar-cava
